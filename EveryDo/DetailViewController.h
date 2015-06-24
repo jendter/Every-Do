d5e01300d043cb77b46d234486455f89e0b14696
Josh Endter <jendter@users.noreply.github.com>
2015-06-24 15:52:13 -0400

    Detail View controller should be passed a Todo object, not an object of type id.

--- EveryDo/DetailViewController.h
+++ EveryDo/DetailViewController.h
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class Todo;
+
 @interface DetailViewController : UIViewController
 
-@property (strong, nonatomic) id detailItem;
+@property (strong, nonatomic) Todo *detailItem;
 //@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
--- EveryDo/DetailViewController.m
+++ EveryDo/DetailViewController.m
@@ -17,7 +17,7 @@
 
 #pragma mark - Managing the detail item
 
-- (void)setDetailItem:(id)newDetailItem {
+- (void)setDetailItem:(Todo *)newDetailItem {
     if (_detailItem != newDetailItem) {
         _detailItem = newDetailItem;
             
@@ -29,7 +29,6 @@
 - (void)configureView {
     // Update the user interface for the detail item.
     if (self.detailItem) {
-        //self.detailDescriptionLabel.text = [self.detailItem description];
         Todo *currentTodoItem = self.detailItem;
         self.titleLabel.text = currentTodoItem.title;
         self.descriptionLabel.text = currentTodoItem.todoDescription;

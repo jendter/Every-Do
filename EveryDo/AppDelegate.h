7725cd6bbdb0e02fc267007dbd0ea107758d927c
Josh Endter <jendter@users.noreply.github.com>
2015-06-24 11:01:06 -0400

    Initial Commit

---
+++ EveryDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EveryDo
+//
+//  Created by Josh Endter on 6/24/15.
+//  Copyright (c) 2015 Josh Endter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Josh Endter on 6/24/15.
+//  Copyright (c) 2015 Josh Endter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
